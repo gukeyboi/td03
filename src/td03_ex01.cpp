70e11e769926506b9128fd499232edbbd055ea68
gukeyboi <lla.kowalik.lain@gmail.com>
2026-03-25 09:44:53 +0100

    fix cmakelists + ex01

---
+++ src/td03_ex01.cpp
@@ -0,0 +1,36 @@
+
+#include <vector>
+#include <iostream>
+#include <utility>  
+
+void bubble_sort(std::vector<int> &vec)
+{
+    size_t n = vec.size();
+    for (size_t i = 0; i + 1 < n; ++i) {
+        bool swapped = false;
+        for (size_t j = 1; j < n - i; ++j) {
+            if (vec[j - 1] > vec[j]) {
+                std::swap(vec[j - 1], vec[j]);
+                swapped = true;
+            }
+        }
+        if (!swapped)
+            break;
+    }
+}
+
+int main()
+{
+    std::vector<int> array{12, 3, 7, 5, 6};
+    bubble_sort(array);
+    for (int x : array)
+        std::cout << x << ' ';
+    std::cout << std::endl;
+
+    return 0;
+}
+
+
+
+
+
