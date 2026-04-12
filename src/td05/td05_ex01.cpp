8f859a52d73308791e6b75b497b67bcae69d9124
gukeyboi <lla.kowalik.lain@gmail.com>
2026-04-12 16:53:56 +0200

    exo 1 - TD5

--- src/td05/td05_ex01.cpp
+++ src/td05/td05_ex01.cpp
@@ -1,36 +1,40 @@
-
 #include <vector>
+#include <algorithm>
 #include <iostream>
-#include <utility>  
 
-void bubble_sort(std::vector<int> &vec)
-{
-    size_t n = vec.size();
-    for (size_t i = 0; i + 1 < n; ++i) {
-        bool swapped = false;
-        for (size_t j = 1; j < n - i; ++j) {
-            if (vec[j - 1] > vec[j]) {
-                std::swap(vec[j - 1], vec[j]);
-                swapped = true;
-            }
-        }
-        if (!swapped)
-            break;
+size_t folding_string_hash(std::string const& s, size_t max) {
+    size_t total = 0;
+    for (size_t i = 0; i < s.size(); i++)
+    {
+        total += int(s[i]);
+        total = total % max;
     }
+    return total;
+    
 }
 
-int main()
-{
-    std::vector<int> array{12, 3, 7, 5, 6, 9, 2, 98};
-    bubble_sort(array);
-    for (int x : array)
-        std::cout << x << ' ';
-    std::cout << std::endl;
-
-    return 0;
+size_t folding_string_ordered_hash(std::string const& s, size_t max) {
+    size_t total = 0;
+    for (size_t i = 0; i < s.size(); i++)
+    {
+        total += (int(s[i]) * (i + 1));
+        total = total % max;
+    }
+    return total;
+    
 }
 
+enum Direction {
+    Haut,
+    Bas,
+    Gauche,
+    Droite
+}
 
+struct Position {
+    
+}
 
-
-
+int main() {
+    std::cout << folding_string_ordered_hash("abc", 2000000000) << std::endl;
+}
