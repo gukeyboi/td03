62f2d2982bdb178d5eb66e138ad944f8061ead20
gukeyboi <lla.kowalik.lain@gmail.com>
2026-04-12 19:20:34 +0200

    ex02_question2 !!

--- src/td05/td05_ex02.cpp
+++ src/td05/td05_ex02.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <fstream>
 
 size_t folding_string_hash(std::string const& s, size_t max) {
     size_t total = 0;
@@ -33,12 +34,15 @@ enum Direction {
     Droite
 };
 
+
+
 struct Position {
     int x;
     int y;
 
     Position() : x(0), y(0) {}
     Position(int x, int y) : x(x), y(y) {}
+Position(size_t x, size_t y) : x(x), y(y) {}
 
     Position& operator+=(Position const& a) {
         x += a.x;
@@ -69,6 +73,13 @@ struct Position {
     };
 };
 
+struct Input_Structure {
+    Position gardePos;
+    Direction gardeDir;
+    std::vector<Position> obstacles;
+    size_t mapWidth;
+    size_t mapHeight;
+};
 
 
 bool operator==(Position const& a, Position const& b) {
@@ -101,10 +112,121 @@ std::ostream& operator<<(std::ostream& os, Position const& c) {
     return os << "coordonnée x =" << c.x << "coordonnée y =" << c.y;
 }
 
+Input_Structure read_input(std::istream& input_stream) {
+   
+    Input_Structure map;
+    
+     map.mapHeight = 0;
+
+    size_t rowIndex = 0;
+
+    for (std::string line{}; std::getline(input_stream, line, '\n') and line != "";) {
+        map.mapWidth = line.size();
+        for (size_t i = 0; i < line.size(); i++)
+        {   
+            switch (line[i])
+            {
+            case '#':
+                map.obstacles.push_back(Position{i, rowIndex});
+                break;
+
+            case '^':
+                map.gardePos = Position{i, rowIndex};
+                map.gardeDir = Direction::Haut;
+            break;
+
+            case '>':
+                map.gardePos = Position{i, rowIndex};
+                map.gardeDir = Direction::Droite;
+            break;
+
+            case 'v':
+                map.gardePos = Position{i, rowIndex};
+                map.gardeDir = Direction::Bas;
+            break;
+
+            case '<':
+                map.gardePos = Position{i, rowIndex};
+                map.gardeDir = Direction::Gauche;
+            break;
+            
+            default:
+                continue;;
+            }
+            
+        }
+        rowIndex++;
+        
+    }
+    map.mapHeight = rowIndex;
+    return map;
+}
+
+bool isObstacle(const std::vector<Position>& vecteur, const Position& cible) {
+
+    auto it = std::find(vecteur.begin(), vecteur.end(), cible);
+    
+    return it != vecteur.end();
+}
+
+void printMap(Input_Structure const& mapimap) {
+    for (size_t i = 0; i < mapimap.mapHeight; i++)
+    {
+        for (size_t j = 0; j < mapimap.mapWidth; j++)
+        {
+            if (isObstacle(mapimap.obstacles, Position {j, i})) {
+                std::cout << "#";
+            }
+
+            else if (Position{j, i} == mapimap.gardePos) {
+                switch (mapimap.gardeDir)
+                {
+                case Direction::Haut:
+                    std::cout << "^";
+                    break;
+
+                case Direction::Droite:
+                    std::cout << ">";
+                    break;    
+
+                case Direction::Bas:
+                    std::cout << "v";
+                    break;
+                    
+                case Direction::Gauche:
+                    std::cout << "<";
+                    break;   
+                
+                default:
+                    break;
+                }
+            }
+
+            else {
+                std::cout << ".";
+            }
+        }
+        std::cout << std::endl;
+        
+    }
+    
+}
 
 int main() {
-    Position endroit {3, 6};
-    Position endroit2 {3, 6};
-    endroit2 += endroit;
-    std::cout << endroit2 << std::endl;
+
+    std::ifstream file("../src/td05/guard_patrol.txt");
+
+    if (!file) {
+        std::cerr << "Erreur fichier" << std::endl;
+        return 1;
+    }
+
+    Input_Structure map = read_input(file);
+
+    printMap(map);
+
+    //Position endroit {3, 6};
+    //Position endroit2 {3, 6};
+    //endroit2 += endroit;
+    //std::cout << endroit2 << std::endl;
 }
